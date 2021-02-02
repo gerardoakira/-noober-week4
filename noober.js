cbe7b8b107b66a906563a3174955b7e65d1dd486
gerardoakira <gerardoakira@hotmail.com>
2021-02-01 18:08:22 -0600

    Final

--- noober.js
+++ noober.js
@@ -34,7 +34,7 @@ async function pageLoaded() {
   function rideDisplay(ride) {
     
     let outputElement = document.querySelector('.rides')
-    outputElement.insertAdjacentHTML('beforeend', `
+      outputElement.insertAdjacentHTML('beforeend', `
       <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
       <i class="fas fa-car-side"></i>
       <span>${levelOfService(ride)}</span>
@@ -49,25 +49,25 @@ async function pageLoaded() {
       <div class="border-4 border-purple-500 p-4 my-4 text-left">
         <div class="flex">
           <div class="w-1/2">
-            <h2 class="text-2xl py-1">${fullName(ride[0])}</h2>
-            <p class="font-bold text-gray-600">${ride[0].passengerDetails.phoneNumber}</p>
+            <h2 class="text-2xl py-1">${fullName(ride[i])}</h2>
+            <p class="font-bold text-gray-600">${ride[i].passengerDetails.phoneNumber}</p>
           </div>
           <div class="w-1/2 text-right">
           <span class="rounded-xl bg-purple-600 text-white p-2">
-              ${numberOfPassengers(ride[0])}
+              ${numberOfPassengers(ride[i])}
             </span>
           </div>
         </div>
         <div class="mt-4 flex">
           <div class="w-1/2">
             <div class="text-sm font-bold text-gray-600">PICKUP</div>
-            <p>${ride[0].pickupLocation.address}</p>
-            <p>${ride[0].pickupLocation.city}, ${ride[0].pickupLocation.state} ${ride[0].pickupLocation.zip}</p>
+            <p>${ride[i].pickupLocation.address}</p>
+            <p>${ride[i].pickupLocation.city}, ${ride[i].pickupLocation.state} ${ride[i].pickupLocation.zip}</p>
           </div>
           <div class="w-1/2">
             <div class="text-sm font-bold text-gray-600">DROPOFF</div>
-            <p>${ride[0].dropoffLocation.address}</p>
-            <p>${ride[0].dropoffLocation.city}, ${ride[0].dropoffLocation.state} ${ride[0].dropoffLocation.zip}</p>
+            <p>${ride[i].dropoffLocation.address}</p>
+            <p>${ride[i].dropoffLocation.city}, ${ride[i].dropoffLocation.state} ${ride[i].dropoffLocation.zip}</p>
           </div>
         </div>
       </div>
@@ -78,25 +78,25 @@ async function pageLoaded() {
         <div class="border-4 border-gray-900 p-4 my-4 text-left">
           <div class="flex">
             <div class="w-1/2">
-              <h2 class="text-2xl py-1">${fullName(ride[0])}</h2>
-              <p class="font-bold text-gray-600">${ride[0].passengerDetails.phoneNumber}</p>
+              <h2 class="text-2xl py-1">${fullName(ride[i])}</h2>
+              <p class="font-bold text-gray-600">${ride[i].passengerDetails.phoneNumber}</p>
             </div>
             <div class="w-1/2 text-right">
               <span class="rounded-xl bg-gray-600 text-white p-2">
-                ${numberOfPassengers(ride[0])}
+                ${numberOfPassengers(ride[i])}
               </span>
             </div>
           </div>
           <div class="mt-4 flex">
             <div class="w-1/2">
               <div class="text-sm font-bold text-gray-600">PICKUP</div>
-              <p>${ride[0].pickupLocation.address}</p>
-              <p>${ride[0].pickupLocation.city}, ${ride[0].pickupLocation.state} ${ride[0].pickupLocation.zip}</p>
+              <p>${ride[i].pickupLocation.address}</p>
+              <p>${ride[i].pickupLocation.city}, ${ride[i].pickupLocation.state} ${ride[i].pickupLocation.zip}</p>
             </div>
             <div class="w-1/2">
               <div class="text-sm font-bold text-gray-600">DROPOFF</div>
-              <p>${ride[0].dropoffLocation.address}</p>
-              <p>${ride[0].dropoffLocation.city}, ${ride[0].dropoffLocation.state} ${ride[0].dropoffLocation.zip}</p>
+              <p>${ride[i].dropoffLocation.address}</p>
+              <p>${ride[i].dropoffLocation.city}, ${ride[i].dropoffLocation.state} ${ride[i].dropoffLocation.zip}</p>
             </div>
           </div>
         </div>
